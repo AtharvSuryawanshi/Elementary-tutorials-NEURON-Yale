0bfe47384372d65aee6c54169b1bca1d5d66db44
AtharvSuryawanshi <133670200+AtharvSuryawanshi@users.noreply.github.com>
2024-05-09 01:38:22 +0530

    Create CurrentClampWithMOD.py

---
+++ Custom_properties/CurrentClampWithMOD.py
@@ -0,0 +1,35 @@
+from neuron import h 
+from neuron.units import mV, ms
+import matplotlib.pyplot as plt
+
+if h.nrn_load_dll(insert path for nrnmech.dll file):
+  print("DLL file loaded")
+
+soma = h.Section(name='soma')
+soma.L = 18.8  # Length of the soma
+soma.Ra = 123  # Axial resistance of the soma
+soma.diam = 18.8  # Diameter of the soma
+soma.nseg = 1  # Number of segments in the soma
+
+soma.insert('hhdemo')  # Insert our custom mechanism
+stamp = 0.1  # Amplitude of the current clamp stimulus
+stim = h.IClamp(soma(0.5))  # Create a current clamp stimulus at the center of the soma
+
+stim.delay = 100  # Delay before the stimulus starts
+stim.dur = 500  # Duration of the stimulus
+stim.amp = stamp  # Amplitude of the stimulus
+
+tstop = 1000  # Duration of the simulation
+
+v = h.Vector().record(soma(0.5)._ref_v)  # Record the membrane potential at the center of the soma
+t = h.Vector().record(h._ref_t)  # Record the time
+
+h.load_file("stdrun.hoc")  # Load the standard run library
+h.finitialize(-65 * mV)  # Set the initial membrane potential
+h.continuerun(tstop * ms)  # Run the simulation for the specified duration
+
+plt.figure()
+plt.plot(t, v)
+plt.xlabel('t (ms)')
+plt.ylabel('v (mV)')
+plt.show()
